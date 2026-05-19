4cd6363b95d5cdf9e698614609a2fe069791ea6c
HB chumbe <hbchumbe@gmail.com>
2026-05-19 17:57:35 -0500

    Agrega primerScript y practicasString 3 ejercicios simples de uso de strings

---
+++ practicasString.py
@@ -0,0 +1,24 @@
+
+##practica de string con python
+##practica 01
+# Crea un código que imprima en pantalla la siguiente expresión 
+# (pero usando print una sola vez):
+# Línea 1
+# Línea 2
+# Línea 3
+
+print("Línea 1\nLínea 2\nLínea 3")
+
+##practica 02
+# Crea un código que imprima en pantalla la siguiente expresión.
+# A       B       C
+# D       E       F
+# G       H       I
+
+print("A\tB\tC\nD\tE\tF\nG\tH\tI")
+##practica 03
+# Crea un código que imprima en pantalla la siguiente expresión:
+
+# Barra Normal: /
+# Barra Invertida: \
+print("Barra Normal: /\nBarra Invertida: \\")
---
+++ primerScript.py
@@ -0,0 +1,4 @@
+print("Hello, World!")
+name = input("Enter his Name : ")
+print("Hello, " + name + "! Welcome to Python programming.")
+print(f"Goodbye, {name}!")
