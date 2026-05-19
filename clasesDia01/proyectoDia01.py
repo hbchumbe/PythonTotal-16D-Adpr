3b9f073364dc548e95c52c8d94e6be93df51cf01
HB chumbe <hbchumbe@gmail.com>
2026-05-19 18:45:19 -0500

    final de la clase 01 , dia uno terminado!

---
+++ clasesDia01/proyectoDia01.py
@@ -0,0 +1,5 @@
+print("El nombre de la cerveceria \nes "
+      +input("ingresa el nombre de tu deporte favorito: ") + " "
+      +input("ingresa el nombre de tu marca de ropa favorita: ")+"\nFelicitaciones") 
+
+
