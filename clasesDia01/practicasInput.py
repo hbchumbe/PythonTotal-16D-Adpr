21d1f3f5a393d6992e720eb3397dec9aa9d85f51
HB chumbe <hbchumbe@gmail.com>
2026-05-19 18:34:37 -0500

    Organiza el proyecto: mueve primerScript a la carpeta claseDia01

---
+++ clasesDia01/practicasInput.py
@@ -0,0 +1,25 @@
+# Práctica Input 1
+# Crea un código que le permita ingresar una respuesta al usuario, 
+# haciéndole la siguiente pregunta:
+# ¿Qué estás estudiando?
+print(input("¿Qué estás estudiando? "))   
+
+# Práctica Input 2
+# Crea un código que le permita ingresar una respuesta al usuario,
+# haciéndole la siguiente pregunta:
+
+# ¿En qué país vives?
+
+print(input("¿En qué país vives?"))   
+
+
+# Práctica Input 3
+# Crea un código que muestre en pantalla el nombre completo del usuario, 
+# permitiéndole ingresar su nombre y apellido con las siguientes instrucciones:
+
+# Escribe tu nombre:
+# Escribe tu apellido:
+
+# El código debe poder imprimir en pantalla el nombre y apellido del usuario, 
+# separados por un espacio
+print(input("Escribe tu nombre: ") + " " + input("Escribe tu apellido: "))
