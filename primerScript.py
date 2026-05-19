0bb88feaf153c4bd441f982627ee329dacc7b125
HB chumbe <hbchumbe@gmail.com>
2026-05-19 18:01:51 -0500

    comentarios faltante en el primer archivo

--- primerScript.py
+++ primerScript.py
@@ -2,3 +2,5 @@ print("Hello, World!")
 name = input("Enter his Name : ")
 print("Hello, " + name + "! Welcome to Python programming.")
 print(f"Goodbye, {name}!")
+# primer archivo de python del curso algo simple como python
+
